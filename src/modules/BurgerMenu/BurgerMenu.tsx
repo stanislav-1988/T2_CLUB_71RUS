2c1fdc744f3897ecfa64c286fd062e5819813c2d
Stanislav <d08121988sv@gmail.com>
2025-09-22 17:38:56 +0300

    fix: sign-in form

--- src/modules/BurgerMenu/BurgerMenu.tsx
+++ src/modules/BurgerMenu/BurgerMenu.tsx
@@ -1,24 +1,85 @@
 import { observer } from 'mobx-react-lite';
 import React, { FC } from 'react';
+import ReactDOM from 'react-dom';
+import { useNavigate } from 'react-router-dom';
 
+import { ROUTES } from '../../providers';
 import myStore from '../../store/myStore';
 import styles from './BurgerMenu.module.scss';
 
 export const BurgerMenu: FC = observer(() => {
   const { openMenu, setOpenMenu } = myStore;
+  const navigate = useNavigate();
+  const portal = document.getElementById('portal');
 
-  return (
+  const handleNavigate = (route: string) => {
+    setTimeout(() => {
+      setOpenMenu(false);
+      navigate(route);
+    }, 1000);
+  };
+
+  return portal && ReactDOM.createPortal(
     <div className={styles.menu}>
       <input checked={openMenu} type="checkbox" id="burger-checkbox" className={styles.burgerCheckbox} />
       <label onClick={() => setOpenMenu(!openMenu)} htmlFor="burger-checkbox" className={styles.burger} />
       <ul className={styles.menuList}>
-        <li><button type="button" className={styles.menuItem}>Личный кабинет</button></li>
-        <li><button type="button" className={styles.menuItem}>Фото встреч</button></li>
-        <li><button type="button" className={styles.menuItem}>Видео</button></li>
-        <li><button type="button" className={styles.menuItem}>Наш мерч</button></li>
-        <li><button type="button" className={styles.menuItem}>Полезные ссылки</button></li>
-        <li><button type="button" className={styles.menuItem}>Контакты</button></li>
+        <li>
+          <button
+            onClick={() => handleNavigate(ROUTES.AUTHORIZATION_ROUTE)}
+            type="button"
+            className={styles.menuItem}
+          >
+            Личный кабинет
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => handleNavigate('')}
+            type="button"
+            className={styles.menuItem}
+          >
+            Фото встреч
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => handleNavigate('')}
+            type="button"
+            className={styles.menuItem}
+          >
+            Видео
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => handleNavigate('')}
+            type="button"
+            className={styles.menuItem}
+          >
+            Наш мерч
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => handleNavigate('')}
+            type="button"
+            className={styles.menuItem}
+          >
+            Полезные ссылки
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => handleNavigate('')}
+            type="button"
+            className={styles.menuItem}
+          >
+            Контакты
+          </button>
+        </li>
       </ul>
-    </div>
+    </div>,
+    portal,
   );
 });
